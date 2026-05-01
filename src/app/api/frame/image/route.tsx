5c86fee53456432bed4963134c5293ba806cc5bf
gblinproject <gblin.protocol@proton.me>
2026-05-01 20:38:17 +0200

    Update route.tsx

--- src/app/api/frame/image/route.tsx
+++ src/app/api/frame/image/route.tsx
@@ -122,7 +122,7 @@ export async function GET() {
     ),
     {
       width: 1200,
-      height: 630,
+      height: 800,
       headers: {
         "Cache-Control": "public, s-maxage=60, stale-while-revalidate=300",
       },
